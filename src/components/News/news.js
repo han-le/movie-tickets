961abd7be65dbd3d055ed4110e0c1cdf5089cf78
han-le <58657916+han-le@users.noreply.github.com>
2021-01-10 01:00:14 +0700

    render news
    loop to render news

--- src/components/News/news.js
+++ src/components/News/news.js
@@ -3,15 +3,60 @@ import "./news_style.css"
 import NewsCard from "../NewsCard/news-card";
 
 class News extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            news_data : [
+                {
+                    id: 1,
+                    image : "https://cdn.asiatatler.com/asiatatler/i/hk/2021/01/07131141-dune-2020-1200-1200-675-675-crop-000000_cover_1200x675.jpg",
+                    title : "Film festival invites fans to watch movies alone on a Swedish island",
+                    intro: "Like many recent film festivals, Göteborg festival is going digital in the wake of the Covid-19 pandemic, but organizers decided to offer one festival..."
+                },
+                {
+                    id: 2,
+                    image: "https://media.wonderlandmagazine.com/uploads/2021/01/MARAMA_6467.jpg",
+                    title: "The Watch‘s tough-talking crime-fighter spills all on Terry Pratchett’s lawless dystopia",
+                    intro: "Marama Corlett is an on-screen talent not to be underestimated. The multi-hyphenate actor has already tussled with the best of them"
+                },
+                {
+                    id: 3,
+                    image: "https://pyxis.nymag.com/v1/imgs/5c0/59d/d2887f10e28e8229052048a173bb905dda-27-dc-feature.2x.rvertical.w512.jpg",
+                    title: "DC Rethinks Its Universe",
+                    intro: "While DC Entertainment has won big in comics, TV, and games, they’ve struggled at the movies."
+                },
+                {
+                    id: 4,
+                    image: "https://static.standard.co.uk/s3fs-public/thumbnails/image/2019/10/09/21/gettyimages-1180069956.jpg",
+                    title: "Angelina Jolie, Elle Fanning and stars hit London for the Maleficent European premiere",
+                    intro: "Angelina Jolie is back for round two with Disney's Sleeping Beauty villain Maleficent, as the star and other A-listers including Elle Fanning"
+                },
+                {
+                    id: 5,
+                    image: "https://cdn3.movieweb.com/i/article/Pq7YQ35n84Z79Qw8h3xV3tgQphV9Xp/1200:100/Soul-Trailer-Disney-Plus-Pixar-Christmas-Release-Date.jpg",
+                    title: "Soul Voice Cast: Who's Voicing Who In The Pixar Movie",
+                    intro: "It was announced earlier this week that Disney is debuting their latest Pixar animated adventure direct to Disney+."
+                },
+                {
+                    id: 6,
+                    image: "https://www.cheatsheet.com/wp-content/uploads/2019/10/chris-hemsworth-1024x683.jpg",
+                    title: "Will Thor Stay Fat After ‘Avengers: Endgame?’",
+                    intro: "Avengers: Endgame featured Thor (Chris Hemsworth) looking not his usual self. The typically ripped Asgardian is shown getting less fit: Simply put, Thor is fluffy throughout the film."
+                }
+            ]
+        }
+    }
+
+    renderNews = () => {
+        return this.state.news_data.map((item)=>{
+            return <NewsCard key={item.id} news={item} />
+        })
+    }
+
     render() {
         return (
             <div className="news_row row">
-                <NewsCard />
-                <NewsCard />
-                <NewsCard />
-                <NewsCard />
-                <NewsCard />
-                <NewsCard />
+                {this.renderNews()}
             </div>
         );
     }
--- src/components/NewsCard/news-card.js
+++ src/components/NewsCard/news-card.js
@@ -2,27 +2,20 @@ import React, {Component} from 'react';
 import "./news-card.css";
 
 class NewsCard extends Component {
-    image = "https://cdn.asiatatler.com/asiatatler/i/hk/2021/01/07131141-dune-2020-1200-1200-675-675-crop-000000_cover_1200x675.jpg";
+
+    news = this.props.news;
 
     render() {
         return (
             <div className="news__card col-md-4">
                 <div className="news__wrapper"
-                     style={{background: `url(${this.image}) center/cover no-repeat`}}>
-                    <div className="news__header">
-                        <div className="date">
-                            <span className="day">12</span>
-                            <span className="month">Aug</span>
-                            <span className="year">2016</span>
-                        </div>
-                    </div>
+                     style={{background: `url(${this.news.image}) center/cover no-repeat`}}>
                     <div className="data">
                         <div className="content">
                             <h1 className="title">
-                                <a href="#">Stranger Things: The sound of the Upside Down</a>
+                                <a href="#">{this.news.title}</a>
                             </h1>
-                            <p className="text">The antsy bingers of Netflix will eagerly anticipate the digital
-                                release of the Survive soundtrack, out today.</p>
+                            <p className="text">{this.news.intro}</p>
                             <a href="#" className="button">Read more</a>
                         </div>
                     </div>
